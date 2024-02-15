092325acb37cef34e80e5dcdaffd03259a1b6381
PedroLeuchs <107222047+PedroLeuchs@users.noreply.github.com>
2024-02-15 11:01:37 -0300

    fix: ProgressBar component

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,29 +1,31 @@
-import * as Progress from '@radix-ui/react-progress';
-import { useState, useEffect } from 'react';
+import * as Progress from "@radix-ui/react-progress";
+import { useState, useEffect } from "react";
 
 export default function ProgressBar() {
-  const [progress, setProgress] = useState(50);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setProgress((prev) => (prev >= 100 ? 0 : prev + 1));
-  //   }, 100);
-  //   return () => clearInterval(interval);
-  // }, []);
+  const [progress, setProgress] = useState(0);
+  const meta = 75;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) =>
+        prev == meta ? prev : prev > meta ? prev - 1 : prev + 1
+      );
+    }, 25);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className='flex flex-col items-center justify-center'>
+    <div className="flex flex-col items-center justify-center">
       <Progress.Root
-        className='relative overflow-hidden bg-white rounded-full w-48 h-5'
-        style={{ transform: 'translateZ(0)' }}
+        className="relative overflow-hidden bg-white rounded-full w-48 h-5"
+        style={{ transform: "translateZ(0)" }}
       >
         <Progress.Indicator
-          className='bg-purple-600 w-full h-full transition-transform duration-500 ease-in-out'
+          className="bg-purple-600 w-full h-full transition-transform duration-25 ease-in-out"
           style={{ transform: `translateX(-${100 - progress}%)` }}
         />
       </Progress.Root>
-      <span className='font-medium text-white '>Meta de Fretes</span>
-      <span className='font-medium text-white '>{progress} %</span>
+      <span className="font-medium text-white ">Meta de Fretes</span>
+      <span className="font-medium text-white ">{progress} %</span>
     </div>
   );
 }
