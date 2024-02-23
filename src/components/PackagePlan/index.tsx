3f4eb92f63285e445285f04284b6ce9a83a9095e
Pedro Leuchs <pedroleuchs01@gmail.com>
2024-02-23 16:27:48 -0300

    Feat: toggleButtonPage4

--- src/components/PackagePlan/index.tsx
+++ src/components/PackagePlan/index.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
-import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
-import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
-import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
-import Stack from '@mui/material/Stack';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import * as React from "react";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
+import Stack from "@mui/material/Stack";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 export default function PackagePlan() {
-  const [alignment, setAlignment] = React.useState('left');
+  const [alignment, setAlignment] = React.useState("left");
 
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
@@ -18,12 +18,23 @@ export default function PackagePlan() {
   };
 
   const children = [
-    <ToggleButton className='w-full' value='right' key='right'>
-      teste0
-    </ToggleButton>,
-    <ToggleButton className='w-full' value='justify' key='justify'>
-      teste
-    </ToggleButton>,
+    <ToggleButton
+      sx={{
+        backgroundColor:
+          alignment === "justify" ? "#yourSelectedColor" : "#yourDefaultColor",
+        "&:hover": {
+          backgroundColor: "#yourHoverColor",
+        },
+      }}
+      className="w-full h-full"
+      value="right"
+      key="right"
+    ></ToggleButton>,
+    <ToggleButton
+      className="w-full h-full"
+      value="justify"
+      key="justify"
+    ></ToggleButton>,
   ];
 
   const control = {
@@ -33,13 +44,12 @@ export default function PackagePlan() {
   };
 
   return (
-    <Stack spacing={2} alignItems='center' className='w-full'>
+    <Stack spacing={2} alignItems="center" className="w-full">
       <ToggleButtonGroup
-        
-        className='w-full p-5 h-48'
-        size='large'
+        className="w-full h-48 "
+        size="large"
         {...control}
-        aria-label='Large sizes'
+        aria-label="Large sizes"
       >
         {children}
       </ToggleButtonGroup>
