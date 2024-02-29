b63d151d61a823bcc7aa484694882123106b8d5b
Pedro Leuchs <pedroleuchs01@gmail.com>
2024-02-29 09:22:56 -0300

    Feat: create a Tabs antd

--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -1,26 +1,46 @@
 "use client";
 import "@/styles/main.css";
-
+import { Tabs } from "antd";
+import type { TabsProps } from "antd";
 import React from "react";
-import ModalComponent from "@/components/Modal";
-import Loading from "@/components/Loading";
-import ProgressBarAntd from "@/components/newProgressBar";
-import UserNotifyBar from "@/components/UserNotifyBar";
-import AdvancedPages from "@/components/AdvancedPages";
-import Page3 from "../CriarFrete/page3";
 
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 const page = () => {
-  const {
-    register,
-    control,
-    formState: { errors },
-  } = useForm();
+  const items: TabsProps["items"] = [
+    {
+      key: "1",
+      label: "Ativos",
+      children: "Content of Tab Pane 1",
+    },
+    {
+      key: "2",
+      label: "Inativos",
+      children: "Content of Tab Pane 2",
+    },
+  ];
+  const onChange = (key: string) => {
+    console.log(key);
+  };
   return (
-    <div className="w-full h-full flex items-start justify-start p-20">
-      <ProgressBarAntd meta={100} percentProgress={50} />
+    <div className="w-full h-full flex items-start justify-start p-10 bg-white">
+      <Tabs
+        
+        defaultActiveKey="1"
+        type="card"
+        size={"large"}
+        items={new Array(2).fill(null).map((_, i) => {
+          const id = String(i + 1);
+          return {
+            label: items.map((item) => item.label)[i] || `Tab ${id} `,
+            key: id,
+            children:
+              `pagina dos fretes ` + items.map((item) => item.label)[i] ||
+              `Tab ${id}`,
+          };
+        })}
+      />
     </div>
   );
 };
