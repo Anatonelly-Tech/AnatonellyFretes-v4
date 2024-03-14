736f3f96a2cccfb2d2185a7fe88e71fb80f70e4d
Crautor <vitor.dallanol@gmail.com>
2024-03-14 12:03:43 -0300

    Feat: Link Cadastro com o DB

--- src/app/public/Cadastro/page.tsx
+++ src/app/public/Cadastro/page.tsx
@@ -1,146 +1,150 @@
-"use client";
-import { useSession } from "next-auth/react";
-import InputLogin from "@/components/InputLogin/Index";
-import React, { ChangeEvent, useRef } from "react";
-import { useForm, Controller } from "react-hook-form";
-import { getCEP } from "@/services/viacep";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { DevTool } from "@hookform/devtools";
-import { FaUserCircle } from "react-icons/fa";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { userValidationSchema } from "@/utils/userValidation";
-import { FaSearch } from "react-icons/fa";
+'use client';
+// Libs
+import React, { useState, ChangeEvent, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { DevTool } from '@hookform/devtools';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-import UploadInput from "@/components/UploadFilesComponent/UploadInput";
-import UploadLabel from "@/components/UploadFilesComponent/UploadLabel";
-import UploadView from "@/components/UploadFilesComponent/UploadView";
-import { HiUserCircle } from "react-icons/hi2";
+// Icons
+import { FaUserCircle } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
+
+// Validation
+import { userValidationSchema } from '@/utils/userValidation';
+
+// Services
+import { postUserFreight } from '@/services/user';
+// Util
+import { getCEP } from '@/services/viacep';
+
+// Component
+import InputLogin from '@/components/InputLogin/Index';
+import UploadInput from '@/components/UploadFilesComponent/UploadInput';
+import UploadLabel from '@/components/UploadFilesComponent/UploadLabel';
+import UploadView from '@/components/UploadFilesComponent/UploadView';
+
 const page = () => {
   const {
     register,
     control,
     getValues,
     setValue,
-
+    handleSubmit,
     formState: { errors, touchedFields },
   } = useForm({
     resolver: yupResolver(userValidationSchema),
   });
-  const [selectedFile, setSelectedFile] = useState("hidden");
+  const [selectedFile, setSelectedFile] = useState('hidden');
   const frameRef = useRef<HTMLImageElement | null>(null);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const { data: session } = useSession();
   const router = useRouter();
+
   const getEndereco = async (cep) => {
     const NovoEndereco = await getCEP(cep);
 
-    setValue("Cidade", NovoEndereco.data.localidade);
-    setValue("Estado", NovoEndereco.data.uf);
-    setValue("Rua", NovoEndereco.data.logradouro);
-    setValue("Bairro", NovoEndereco.data.bairro);
+    setValue('city', NovoEndereco.data.localidade);
+    setValue('state', NovoEndereco.data.uf);
+    setValue('street', NovoEndereco.data.logradouro);
+    setValue('neighborhood', NovoEndereco.data.bairro);
   };
 
   const preview = (event: ChangeEvent<HTMLInputElement>) => {
     if (frameRef.current && event.target.files && event.target.files[0]) {
       frameRef.current.src = URL.createObjectURL(event.target.files[0]);
     }
-    setSelectedFile("flex");
+    setSelectedFile('flex');
   };
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const onSubmit = async (data: any) => {
     try {
-      const response = await signIn("credentials", {
-        redirect: false,
-        email,
-        password,
-      });
-      if (!response?.error) {
-        console.log(session);
-        router.refresh();
-        router.push("/private/Home");
-      } else {
-        setError("email ou senha incorretos");
-      }
-    } catch (error) {
-      console.log("[LOGIN_ERROR]", error);
+      const response = await postUserFreight(data);
+      console.log(response);
+    } catch {
+      console.log('Erro ao cadastrar usuário');
     }
   };
-  //  (
-  //
-  // )
 
   return (
-    <div className="w-screen h-screen bg-white flex items-center justify-center relative -z-10">
+    <div className='w-screen h-screen bg-white flex items-center justify-center relative -z-10'>
       <DevTool control={control} />
       <img
         draggable={false}
-        className="w-full h-full absolute -z-10"
-        src="/bgLogin.png"
-        alt=""
+        className='w-full h-full absolute -z-10'
+        src='/bgLogin.png'
+        alt=''
       />
-      <div className="flex flex-col items-center justify-center gap-5 p-14 bg-gradient-to-t from-purple-400 via-[rgba(60,7,100,0.82)] to-purple-950 rounded-lg shadow-lg shadow-black">
-        <div className="flex items-center w-20 h-20  justify-center rounded-full border-2 border-purple-600">
+      <div className='flex flex-col items-center justify-center gap-5 p-14 bg-gradient-to-t from-purple-400 via-[rgba(60,7,100,0.82)] to-purple-950 rounded-lg shadow-lg shadow-black'>
+        <div className='flex items-center w-20 h-20  justify-center rounded-full border-2 border-purple-600'>
           <UploadView frameRef={frameRef} selectedFile={selectedFile} />
-          {selectedFile !== "hidden" ? null : (
-            <FaUserCircle className="text-zinc-200 w-full h-full" />
+          {selectedFile !== 'hidden' ? null : (
+            <FaUserCircle className='text-zinc-200 w-full h-full' />
           )}
         </div>
-        <form className="w-full h-full flex flex-col items-center justify-around ">
-          <div className="flex items-center justify-between w-full h-full">
-            <div className="flex flex-col items-center gap-5 justify-between">
-              <p className="text-xl text-white font-bold">Dados Pessoais</p>
-              <div className="flex flex-col items-center justify-center gap-5 p-3">
-                <InputLogin
-                  id="NomeCompleto"
-                  register={register}
-                  name="NomeCompleto"
-                  type="text"
-                  label="Nome Completo"
-                />
-
-                <div className="w-full flex items-center justify-center">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          action='#'
+          id='CadastroForm'
+          className='w-full h-full flex flex-col items-center justify-around '
+        >
+          <div className='flex items-center justify-between w-full h-full'>
+            <div className='flex flex-col items-center gap-5 justify-between'>
+              <p className='text-xl text-white font-bold'>Dados Pessoais</p>
+              <div className='flex flex-col items-center justify-center gap-5 p-3'>
+                <div className='w-full flex items-center justify-center'>
                   <InputLogin
-                    id="CPF"
+                    id='NomeCompleto'
                     register={register}
-                    name="CPF"
-                    type="text"
-                    label="CPF"
-                    mask="999.999.999-99"
+                    name='name'
+                    type='text'
+                    label='Nome Completo'
+                  />
+                  <InputLogin
+                    id='Email'
+                    register={register}
+                    name='email'
+                    type='email'
+                    label='Email'
+                  />
+                </div>
+                <div className='w-full flex items-center justify-center'>
+                  <InputLogin
+                    id='CPF'
+                    register={register}
+                    name='cpf'
+                    type='text'
+                    label='CPF'
+                    mask='999.999.999-99'
                   />
 
                   <InputLogin
-                    id="Telefone"
+                    id='Telefone'
                     register={register}
-                    name="Telefone"
-                    type="text"
-                    label="Telefone"
-                    mask="(99) 99999-9999"
+                    name='phone'
+                    type='text'
+                    label='Telefone'
+                    mask='(99) 99999-9999'
                   />
                 </div>
 
-                <div className="w-full flex items-center justify-center">
+                <div className='w-full flex items-center justify-center'>
                   <InputLogin
-                    id="Senha"
+                    id='Senha'
                     register={register}
-                    name="Senha"
-                    type="password"
-                    label="Senha"
+                    name='password'
+                    type='password'
+                    label='Senha'
                   />
                   <InputLogin
-                    id="ConfirmarSenha"
+                    id='ConfirmarSenha'
                     register={register}
-                    name="ConfirmarSenha"
-                    type="password"
-                    label="Confirmar Senha"
+                    name='confirmPassword'
+                    type='password'
+                    label='Confirmar Senha'
                   />
                 </div>
-                <div className="w-full flex items-center justify-center gap-4 p-1">
-                  <UploadLabel htmlFor="picture" />
+                <div className='w-full flex items-center justify-center gap-4 p-1'>
+                  <UploadLabel htmlFor='picture' />
                   <UploadInput
                     selectedFile={selectedFile}
                     register={register}
@@ -152,90 +156,89 @@ const page = () => {
                 </div>
               </div>
             </div>
-            {/* <div className=" "></div> */}
-            <div className="flex flex-col items-center gap-5 justify-between">
-              <p className="text-xl text-white font-bold">Endereço</p>
-              <div className="flex flex-col items-center justify-center gap-5 p-3">
-                <div className="w-full flex items-center justify-center">
+            <div className='flex flex-col items-center gap-5 justify-between'>
+              <p className='text-xl text-white font-bold'>Endereço</p>
+              <div className='flex flex-col items-center justify-center gap-5 p-3'>
+                <div className='w-full flex items-center justify-center'>
                   <InputLogin
-                    id="CEP"
+                    id='CEP'
                     register={register}
-                    name="CEP"
-                    type="text"
-                    label="CEP"
-                    mask="99999-999"
+                    name='cep'
+                    type='text'
+                    label='CEP'
+                    mask='99999-999'
                   />
                   <button
-                    className="w-auto h-9 flex items-center justify-center gap-3 p-3 px-5 rounded-lg text-nowrap text-white bg-purple-600  hover:bg-purple-700 shadow shadow-black"
-                    type="button"
-                    onClick={async () => await getEndereco(getValues("CEP"))}
+                    className='w-auto h-9 flex items-center justify-center gap-3 p-3 px-5 rounded-lg text-nowrap text-white bg-purple-600  hover:bg-purple-700 shadow shadow-black'
+                    type='button'
+                    onClick={async () => await getEndereco(getValues('cep'))}
                   >
                     {/* Buscar Cep */}
                     <FaSearch />
                   </button>
                 </div>
-                <div className="w-full flex items-center justify-center">
+                <div className='w-full flex items-center justify-center'>
                   <InputLogin
-                    id="Cidade"
+                    id='Cidade'
                     register={register}
-                    name="Cidade"
-                    type="text"
-                    label="Cidade"
+                    name='city'
+                    type='text'
+                    label='Cidade'
                   />
                   <InputLogin
-                    id="Estado"
+                    id='Estado'
                     register={register}
-                    name="Estado"
-                    type="text"
-                    label="Estado"
-                  />
-                </div>
-                <div className="w-full flex items-center justify-center">
-                  <InputLogin
-                    id="Rua"
-                    register={register}
-                    name="Rua"
-                    type="text"
-                    label="Rua"
-                  />
-                  <InputLogin
-                    id="Numero"
-                    register={register}
-                    name="Numero"
-                    type="text"
-                    label="Numero"
+                    name='state'
+                    type='text'
+                    label='Estado'
                   />
                 </div>
-                <div className="w-full flex items-center justify-center">
+                <div className='w-full flex items-center justify-center'>
                   <InputLogin
-                    id="Bairro"
+                    id='Rua'
                     register={register}
-                    name="Bairro"
-                    type="text"
-                    label="Bairro"
+                    name='street'
+                    type='text'
+                    label='Rua'
                   />
                   <InputLogin
-                    id="Complemento"
+                    id='Numero'
                     register={register}
-                    name="Complemento"
-                    type="text"
-                    label="Complemento"
+                    name='number'
+                    type='text'
+                    label='Numero'
+                  />
+                </div>
+                <div className='w-full flex items-center justify-center'>
+                  <InputLogin
+                    id='Bairro'
+                    register={register}
+                    name='neighborhood'
+                    type='text'
+                    label='Bairro'
+                  />
+                  <InputLogin
+                    id='Complemento'
+                    register={register}
+                    name='complement'
+                    type='text'
+                    label='Complemento'
                   />
                 </div>
               </div>
             </div>
           </div>
-          <div className="w-full flex items-center justify-between">
+          <div className='w-full flex items-center justify-between'>
             <button
-              onClick={() => router.push("/")}
-              className="w-32 h-9 rounded-lg font-bold text-white bg-purple-600  hover:bg-purple-700 shadow shadow-black"
-              type="button"
+              onClick={() => router.push('/')}
+              className='w-32 h-9 rounded-lg font-bold text-white bg-purple-600  hover:bg-purple-700 shadow shadow-black'
+              type='button'
             >
               Voltar
             </button>
             <button
-              type="submit"
-              className="w-32 h-9 rounded-lg font-bold text-white bg-purple-600  hover:bg-purple-700 shadow shadow-black"
+              type='submit'
+              className='w-32 h-9 rounded-lg font-bold text-white bg-purple-600  hover:bg-purple-700 shadow shadow-black'
             >
               Cadastrar
             </button>
