762ddf8bfcb3cd211068451e764325e754599fa5
Crautor <vitor.dallanol@gmail.com>
2024-03-18 09:06:34 -0300

    feat: add some screens to media on tailwind and make responsive Login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,13 +1,13 @@
-"use client";
-import { useSession } from "next-auth/react";
-import CheckBox from "@/components/CheckBox";
-import InputLogin from "@/components/InputLogin/Index";
-import React from "react";
-import { useForm } from "react-hook-form";
+'use client';
+import { useSession } from 'next-auth/react';
+import CheckBox from '@/components/CheckBox';
+import InputLogin from '@/components/InputLogin/Index';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const page = () => {
   const {
@@ -21,16 +21,16 @@ const page = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState('');
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await signIn("credentials", {
+      const response = await signIn('credentials', {
         redirect: false,
         email,
         password,
@@ -38,84 +38,75 @@ const page = () => {
       if (!response?.error) {
         console.log(session);
         router.refresh();
-        router.push("/private/Home");
+        router.push('/private/Home');
       } else {
-        setError("email ou senha incorretos");
+        setError('email ou senha incorretos');
       }
     } catch (error) {
-      console.log("[LOGIN_ERROR]", error);
+      console.log('[LOGIN_ERROR]', error);
     }
   };
 
   return (
-    <div className="w-screen h-screen bg-white flex items-center justify-center relative -z-10">
+    <div className='w-screen h-screen bg-white flex items-center justify-center relative -z-10'>
       <img
         draggable={false}
-        className="w-full h-full absolute -z-10"
-        src="/bgLogin.png"
-        alt=""
+        className='w-full h-full absolute -z-10'
+        src='/bgLogin.png'
+        alt=''
       />
-      <div className="flex flex-col items-center justify-between gap-5 h-3/4 w-1/4 bg-gradient-to-t from-purple-200 via-[rgba(60,7,100,0.82)] to-purple-950 p-10 rounded-lg shadow-lg shadow-black">
+      <div className='flex flex-col items-center justify-between gap-5 h-3/4 sm:h-auto md:h-auto sm:w-auto md:w-auto lg:w-1/4 bg-gradient-to-t from-purple-200 via-[rgba(60,7,100,0.82)] to-purple-950 p-10 rounded-lg shadow-lg shadow-black'>
         <img
           draggable={false}
-          className="w-52"
-          src="/AnatonellyPc.png"
-          alt=""
+          className='xxs:hidden sm:flex w-52'
+          src='/AnatonellyPc.png'
+          alt=''
         />
         <form
-          className="flex flex-col items-center justify-around h-full w-full"
+          className='flex flex-col items-center justify-around h-full w-full'
           onSubmit={handleLogin}
         >
-          <div className="w-full">
+          <div className='w-full'>
             <InputLogin
-              id="Email"
+              id='Email'
               register={register}
-              name="Email"
-              type="text"
-              label="Email"
+              name='Email'
+              type='text'
+              label='Email'
               onChange={(e) => setEmail(e.target.value)}
               required
             />
             <InputLogin
-              id="Senha"
+              id='Senha'
               register={register}
-              type="password"
-              name="Senha"
-              label="Senha"
+              type='password'
+              name='Senha'
+              label='Senha'
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
-          <div className="flex justify-center items-center w-full">
-            {/* <CheckBox
-              id="ManterConcectado"
-              register={register}
-              label="Manter conectado"
-              value={false}
-              name="ManterConcectado"
-            /> */}
-            <a
-              className="text-zinc-300 hover:text-zinc-500 font-bold underline text-center"
-              href=""
-            >
-              Esqueci minha senha
-            </a>
-          </div>
+          <a
+            className='text-zinc-300 hover:text-zinc-500 font-bold underline text-center'
+            href=''
+          >
+            Esqueci minha senha
+          </a>
           {error && (
-            <span className="text-red-400 text-sm block mt-2">{error}</span>
+            <span className='text-red-400 text-sm block mt-2'>{error}</span>
           )}
           {/* <a href="/private/Home"> */}
           <button
-            type="submit"
-            className="bg-purple-600 hover:bg-purple-700 text-white font-bold px-10 py-2 text-xl rounded-lg shadow-especial shadow-purple-800"
+            type='submit'
+            className='bg-purple-600 hover:bg-purple-700 text-white font-bold px-10 py-2 text-xl rounded-lg shadow-especial shadow-purple-800'
           >
             Entrar
           </button>
           {/* </a> */}
         </form>
         <a
-          className="text-purple-500 hover:text-purple-600 font-bold underline"
-          href=".././public/Cadastro"
+          className='text-purple-500 hover:text-purple-600 font-bold underline'
+          href='.././public/Cadastro'
         >
           Ainda não tem uma conta?
         </a>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,104 +1,114 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      xxs: '280px',
+      xs: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+
     extend: {
       boxShadow: {
-        especial: "rgba(0, 0, 0, 0.35) 0px -50px 36px -28px inset",
+        especial: 'rgba(0, 0, 0, 0.35) 0px -50px 36px -28px inset',
         interno:
-          "rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset",
+          'rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset',
         rounded:
-          "rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px",
+          'rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px',
       },
       backgroundImage: {
-        "anatonelly-bg": 'url("/AnatonellyFretes.png")',
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "lettler-bg": "bg-gradient-to-tl from-black to-purple-600",
+        'anatonelly-bg': 'url("/AnatonellyFretes.png")',
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'lettler-bg': 'bg-gradient-to-tl from-black to-purple-600',
       },
       keyframes: {
         spin: {
-          from: { transform: "rotate(0deg)" },
-          to: { transform: "rotate(360deg)" },
+          from: { transform: 'rotate(0deg)' },
+          to: { transform: 'rotate(360deg)' },
         },
         fadeIn: {
-          from: { opacity: "0", transform: "translateY(-200px) " },
-          to: { opacity: "1", transform: "translateY(0) " },
+          from: { opacity: '0', transform: 'translateY(-200px) ' },
+          to: { opacity: '1', transform: 'translateY(0) ' },
         },
         fadeOut: {
-          from: { opacity: "1", transform: "translateY(0) " },
-          to: { opacity: "0", transform: "translateY(-200px) " },
+          from: { opacity: '1', transform: 'translateY(0) ' },
+          to: { opacity: '0', transform: 'translateY(-200px) ' },
         },
         enterFromRight: {
-          from: { opacity: "0", transform: "translateY(200px) " },
-          to: { opacity: "1", transform: "translateY(0) -translate-x-1/3" },
+          from: { opacity: '0', transform: 'translateY(200px) ' },
+          to: { opacity: '1', transform: 'translateY(0) -translate-x-1/3' },
         },
         enterFromLeft: {
-          from: { opacity: "0", transform: "translateY(200px)  " },
-          to: { opacity: "1", transform: "translateY(0) -translate-x-1/3" },
+          from: { opacity: '0', transform: 'translateY(200px)  ' },
+          to: { opacity: '1', transform: 'translateY(0) -translate-x-1/3' },
         },
         exitToRight: {
-          from: { opacity: "1", transform: "translateY(0) -translate-x-1/3" },
-          to: { opacity: "0", transform: "translateY(200px) " },
+          from: { opacity: '1', transform: 'translateY(0) -translate-x-1/3' },
+          to: { opacity: '0', transform: 'translateY(200px) ' },
         },
         exitToLeft: {
-          from: { opacity: "1", transform: "translateY(0) -translate-x-1/3" },
-          to: { opacity: "0", transform: "translateY(200px) " },
+          from: { opacity: '1', transform: 'translateY(0) -translate-x-1/3' },
+          to: { opacity: '0', transform: 'translateY(200px) ' },
         },
         scaleIn: {
-          from: { opacity: "0", transform: "rotateX(-10deg) scale(0.9)" },
-          to: { opacity: "1", transform: "rotateX(0deg) scale(1)" },
+          from: { opacity: '0', transform: 'rotateX(-10deg) scale(0.9)' },
+          to: { opacity: '1', transform: 'rotateX(0deg) scale(1)' },
         },
         scaleOut: {
-          from: { opacity: "1", transform: "rotateX(0deg) scale(1)" },
-          to: { opacity: "0", transform: "rotateX(-10deg) scale(0.95)" },
+          from: { opacity: '1', transform: 'rotateX(0deg) scale(1)' },
+          to: { opacity: '0', transform: 'rotateX(-10deg) scale(0.95)' },
         },
 
         apperContent: {
-          from: { opacity: "0", transform: "translateY(200px) " },
-          to: { opacity: "1", transform: "translateY(0) " },
+          from: { opacity: '0', transform: 'translateY(200px) ' },
+          to: { opacity: '1', transform: 'translateY(0) ' },
         },
         MoveRight: {
-          from: { transform: "translateX(200px)" },
-          to: { transform: "translateX(0)" },
+          from: { transform: 'translateX(200px)' },
+          to: { transform: 'translateX(0)' },
         },
         MoveLeft: {
-          from: { opacity: "0", transform: "translateX(-200px)" },
-          to: { opacity: "1", transform: "translateX(0)" },
+          from: { opacity: '0', transform: 'translateX(-200px)' },
+          to: { opacity: '1', transform: 'translateX(0)' },
         },
         Apper: {
-          from: { opacity: "0" },
-          to: { opacity: "1" },
+          from: { opacity: '0' },
+          to: { opacity: '1' },
         },
         Desapper: {
-          from: { opacity: "1" },
-          to: { opacity: "0" },
+          from: { opacity: '1' },
+          to: { opacity: '0' },
         },
       },
     },
     animation: {
-      fadeIn: "fadeIn 300ms ease",
-      fadeOut: "fadeOut 300ms ease",
-      scaleIn: "scaleIn 200ms ease",
-      scaleOut: "scaleOut 200ms ease",
-      spin: "spin 2s linear infinite",
-      enterFromLeft: "enterFromLeft 250ms ease",
-      enterFromRight: "enterFromRight 250ms ease",
-      exitToLeft: "exitToLeft 250ms ease",
-      exitToRight: "exitToRight 250ms ease",
-      MoveRight: "MoveRight 1s ease",
-      MoveLeft: "MoveLeft 150ms ease",
-      Apper: "Apper 1s ease",
-      Desapper: "Desapper 1s ease",
+      fadeIn: 'fadeIn 300ms ease',
+      fadeOut: 'fadeOut 300ms ease',
+      scaleIn: 'scaleIn 200ms ease',
+      scaleOut: 'scaleOut 200ms ease',
+      spin: 'spin 2s linear infinite',
+      enterFromLeft: 'enterFromLeft 250ms ease',
+      enterFromRight: 'enterFromRight 250ms ease',
+      exitToLeft: 'exitToLeft 250ms ease',
+      exitToRight: 'exitToRight 250ms ease',
+      MoveRight: 'MoveRight 1s ease',
+      MoveLeft: 'MoveLeft 150ms ease',
+      Apper: 'Apper 1s ease',
+      Desapper: 'Desapper 1s ease',
     },
   },
-  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+  plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
 };
 
 export default config;
