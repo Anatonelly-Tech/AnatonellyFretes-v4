477dd9f89a24e43c04aaea6e2a084b1f132abb0b
Crautor <vitor.dallanol@gmail.com>
2024-03-20 08:33:55 -0300

    Fix: fix test to next can build the app

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,14 +3,12 @@
 import "../../styles/main.css";
 
 import React, { useState } from "react";
-import NavBarMobile from "@/components/NavBarMobile";
 import { NavBar } from "@/components/NavBar";
 
 const page = () => {
   return (
     <div className="p-5 gap-3 w-full h-full flex ">
       <NavBar />
-      {/* <NavBarMobile /> */}
     </div>
   );
 };
